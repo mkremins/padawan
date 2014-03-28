cdf94aef8c813f98f5956624544197616a1dba97
Max Kreminski <maxkreminski@gmail.com>
2014-03-28 09:58:29 -0700

    Make javax.servlet/servlet-api a runtime dependency

--- project.clj
+++ project.clj
@@ -10,9 +10,7 @@
    [org.clojure/data.json "0.2.4"]
    [compojure "1.1.6"]
    [http-kit "2.1.18"]
+   [javax.servlet/servlet-api "2.5"]
    [ring/ring-core "1.2.2"]]
-
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]}}
 
   :main padawan.core)
